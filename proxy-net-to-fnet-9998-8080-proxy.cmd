0fde520d8610a085908f3035dc7d461ab1a0fbb0
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-10-06 16:13:20 +0900

    revise script

--- proxy-net-to-fnet-9998-8080-proxy.cmd
+++ proxy-net-to-fnet-9998-8080-proxy.cmd
@@ -3,8 +3,7 @@
 	set XPORTER_PROXY_FOLDER=work
 	if not exist work mkdir work
 )
-@pause
 @echo %XPORTER_PROXY_FOLDER%
-node net-fnet %XPORTER_PROXY_FOLDER% 9998 proxy 8080
-@pause
+@node net-fnet %XPORTER_PROXY_FOLDER% 9998 proxy 8080
 @popd
+@pause
