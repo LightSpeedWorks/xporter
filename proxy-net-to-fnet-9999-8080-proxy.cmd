a6f3807aa6e9186d993834afc7d4d3554d19db7f
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-02-01 08:19:16 +0900

    add test script

---
+++ proxy-net-to-fnet-9999-8080-proxy.cmd
@@ -0,0 +1,10 @@
+@node -p '\x1b[42m' & cls
+@pushd %~dp0
+@if "%XPORTER_PROXY_FOLDER%" == "" (
+	set XPORTER_PROXY_FOLDER=work
+	if not exist work mkdir work
+)
+@echo %XPORTER_PROXY_FOLDER%
+@node net-fnet %XPORTER_PROXY_FOLDER% 9999 proxy 8080
+@popd
+@pause
